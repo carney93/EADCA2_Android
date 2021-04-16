c61a27438b54b1126346ec54eec5e7520b5a1c9e
X00145679 <davobrowne@gmail.com>
2021-04-16 14:18:41 +0100

    api calling setup

--- app/src/main/java/com/example/upcominggamereleases/MainActivity.java
+++ app/src/main/java/com/example/upcominggamereleases/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.upcominggamereleases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showAllGames(View v) {
+
+        ApiCaller apiCaller = new ApiCaller();
+        //change this
+            try{
+                TableLayout tableLayout = findViewById(R.id.table1);
+                TextView text = new TextView(v.getContext());
+                text.setText(apiCaller.listAll());
+            }catch (Exception e){
+
+            }
     }
 }
